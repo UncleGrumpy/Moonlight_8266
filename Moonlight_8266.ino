f8cdba3dd90146faf4c2f488b3e0ef2073a90cd3
UncleGrumpy <69995513+UncleGrumpy@users.noreply.github.com>
2021-03-10 22:19:40 -0800

    Ignition!
    minor fix.

--- Moonlight_8266.ino
+++ Moonlight_8266.ino
@@ -17,8 +17,8 @@ WebSocketsServer webSocket(81);    // create a websocket server on port 81
 
 File fsUploadFile;                // a File variable to temporarily store the received file
 
-const char *ssid = "Moonbeam";    // The name of the Wi-Fi network that will be created
-const char *password = "sage1120";   // The password required to connect to it, leave blank for an open network
+const char *ssid = "Moonlight";    // The name of the Wi-Fi network that will be created
+const char *password = "";   // The password required to connect to it, leave blank for an open network
 
 const char *OTAName = "moon";           // A hostname and a password for the OTA service
 const char *OTAPassword = "31f2385ba9cc65dba7ccb9aa5c5b7600";     // md5() hash of password
