d2403d7d80e0f98748a4bc106b76fec119be7515
UncleGrumpy <69995513+UncleGrumpy@users.noreply.github.com>
2021-03-14 13:26:52 -0700

    Fixed uplod button.
    The file upload button is now disabled until after a file is actually selected.

--- data/upload.js
+++ data/upload.js
@@ -4,26 +4,44 @@ function fileCheck() {
     if ( inputFile != null) {
         let haveFile = inputFile.fileList;
         if ( haveFile == undefined) {
-//             console.log("No files selected. Upload disabled.");
+            console.log("No files selected. Upload disabled.");
             document.getElementById("upButton").disabled = true;
             document.getElementById("upButton").className = 'disabled';        
         }
         console.log("File selection made. Upload enabled.");
-        // Enable the submit button.
+        //Enable the submit button.
         document.getElementById("upButton").disabled = false;
         document.getElementById("upButton").className = 'enabled';
     }
 }
- 
-//         if ( haveFile != null) {
-//             console.log("File selection made. Upload enabled.");
-//             //Enable the submit button.
-//             document.getElementById("upButton").disabled = false;
-//             document.getElementById("upButton").className = 'enabled';
-//         } else {
-//             console.log("No files selected. Upload disabled.");
-//             document.getElementById("upButton").disabled = true;
-//             document.getElementById("upButton").className = 'disabled';
-//         }
-//     }
-// }
+
+window.addEventListener("load", startup, false);
+function startup() {
+    document.getElementById('upButton').disabled = true;
+    document.getElementById('upButton').className = 'disabled';
+    myFile = document.getElementById('uploads');
+//     console.log("Setting up event listener on " + myFile);
+    myFile.addEventListener("change", updateAll, false);
+    myFile.select();
+}
+function updateAll(event) { // this will run on input.
+    var file = document.getElementById('uploads');
+//     console.log("target is: " + file);
+    let haveFile = file.value;
+//     console.log("got file: " + haveFile);
+    if (haveFile) {
+//         console.log("target is: " + file);
+//         console.log("got file: " + haveFile);
+        console.log("File selection made. Upload enabled.");
+        // Enable the submit button.
+        document.getElementById("upButton").disabled = false;
+        document.getElementById("upButton").className = 'enabled';
+    } else {
+        console.log("No file selection made.");
+        console.log("target is: " + file);
+        console.log("got file: " + haveFile);
+        // Disable the submit button.
+        document.getElementById("upButton").disabled = true;
+        document.getElementById("upButton").className = 'disabled';
+    }
+}
