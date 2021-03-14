ce927e0ad48c1e0e76e2c1aa18a785ee954dc88c
UncleGrumpy <69995513+UncleGrumpy@users.noreply.github.com>
2021-03-14 13:33:10 -0700

    Code cleanup.
    Removed unused functions and commented out sections.

--- data/upload.js
+++ data/upload.js
@@ -1,20 +1,4 @@
 
-function fileCheck() {
-    var inputFile = document.getElementById('uploads');
-    if ( inputFile != null) {
-        let haveFile = inputFile.fileList;
-        if ( haveFile == undefined) {
-            console.log("No files selected. Upload disabled.");
-            document.getElementById("upButton").disabled = true;
-            document.getElementById("upButton").className = 'disabled';        
-        }
-        console.log("File selection made. Upload enabled.");
-        //Enable the submit button.
-        document.getElementById("upButton").disabled = false;
-        document.getElementById("upButton").className = 'enabled';
-    }
-}
-
 window.addEventListener("load", startup, false);
 function startup() {
     document.getElementById('upButton').disabled = true;
